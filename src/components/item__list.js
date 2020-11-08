84d03d7c49f83dadc97fa6b61fa768e2443ab0bb
오형욱 <nenonenoberneno@gmail.com>
2020-11-08 09:46:27 +0900

    image 오류 수정 1

--- src/components/item__list.js
+++ src/components/item__list.js
@@ -15,7 +15,7 @@ export const item = [
         score : "4.5"},
     {
         url : "#", 
-        img : "에이비글.jpg",
+        img : "abigle.jpg",
         alt : "thumnail image",
         brand : "에이비글",
         title : "DEDICATION 핏 보장 루즈핏 MTM 간절기 배색 루즈핏 긴팔 Top_에이비글",
@@ -28,7 +28,7 @@ export const item = [
 
     { 
         url : "#", 
-        img : "아베크조이.jpg",
+        img : "abekjoy.jpg",
         alt : "thumnail image",
         brand : "아베크조이",
         title : "고퀄 울80 여성 골지 반목 반폴라 버튼 단추 사선 니트 티셔츠 가을 겨울 봄 162",
@@ -41,7 +41,7 @@ export const item = [
 
     { 
         url : "#", 
-        img : "데일리히어.jpg",
+        img : "daliyhere.jpg",
         alt : "thumnail image",
         brand : "데일리히어",
         title : "[6COLOR] 쫀쫀 세미크롭 라운드 반팔 티셔츠_데일리히어",
@@ -54,7 +54,7 @@ export const item = [
 
     { 
         url : "#", 
-        img : "데이비쥬.jpg",
+        img : "daybjue.jpg",
         alt : "thumnail image",
         brand : "데이비쥬",
         title : "쏘핫 U넥 쫀쫀 크롭티 (4 color) _데이비쥬",
@@ -67,7 +67,7 @@ export const item = [
 
     { 
         url : "#", 
-        img : "디어모브.jpg",
+        img : "dearmov.jpg",
         alt : "thumnail image",
         brand : "디어모브",
         title : "ggomo 오버핏 꽈배기 케이블 울 라운드 니트_디어모브",
@@ -92,7 +92,7 @@ export const item = [
         score : "4.5"},
     { 
         url : "#", 
-        img : "원더원더.jpg",
+        img : "wonderwonder.jpg",
         alt : "thumnail image",
         brand : "원더원더",
         title : "[1+1 특가!] 크롭 미니 쫀쫀 반팔 티셔츠",
@@ -104,7 +104,7 @@ export const item = [
         score : "4.6"},
     { 
         url : "#", 
-        img : "더뮤즈.jpg",
+        img : "themuse.jpg",
         alt : "thumnail image",
         brand : "더뮤즈",
         title : "[국내생산]데일리 루즈 크롭 긴팔 4컬러_더뮤즈",
@@ -116,7 +116,7 @@ export const item = [
         score : "4.8"},
     { 
         url : "#", 
-        img : "비커밍27.jpg",
+        img : "berkerming27.jpg",
         alt : "thumnail image",
         brand : "비커밍27",
         title : "V 넥 플라워 롱 원피스 _비커밍27",
@@ -128,7 +128,7 @@ export const item = [
         score : "4.7"},
     { 
         url : "#", 
-        img : "헤이레이디.jpg",
+        img : "heylady.jpg",
         alt : "thumnail image",
         brand : "헤이레이디",
         title : "[내추럴♥여유핏NT]알레즈 라운드 니트",
@@ -140,7 +140,7 @@ export const item = [
         score : "4.8"},
     { 
         url : "#", 
-        img : "바이영.jpg",
+        img : "byyoung.jpg",
         alt : "thumnail image",
         brand : "바이영",
         title : "(국내제작 울60) 쓰리버튼 루즈핏 베이직 울자켓 / 자켓_바이영",
@@ -152,7 +152,7 @@ export const item = [
         score : "4.7"},
     { 
         url : "#", 
-        img : "혀나블리.jpg",
+        img : "hyonabuly.jpg",
         alt : "thumnail image",
         brand : "혀나블리",
         title : "여성스러운 무드의 크롭 브이넥니트_혀나블리",
@@ -164,7 +164,7 @@ export const item = [
         score : "4.6"},
     { 
         url : "#", 
-        img : "돈키.jpg",
+        img : "donkey.jpg",
         alt : "thumnail image",
         brand : "돈키",
         title : "[브랜드핫템] sepium bag - D1032 (12color)",
@@ -176,7 +176,7 @@ export const item = [
         score : "4.7"},
     { 
         url : "#", 
-        img : "완스몰.jpg",
+        img : "wansmall.jpg",
         alt : "thumnail image",
         brand : "완스몰",
         title : "[주문폭주] [오늘출발] 모먼트 브이넥 가디건",
@@ -188,7 +188,7 @@ export const item = [
         score : "4.8"},
     { 
         url : "#", 
-        img : "잇쇼제이.jpg",
+        img : "itshowj.jpg",
         alt : "thumnail image",
         brand : "잇쇼제이",
         title : "[롱한기장감] 하이 롱 세미와이드 데님 팬츠_잇쇼제이",
@@ -200,7 +200,7 @@ export const item = [
         score : "4.7"},
     { 
         url : "#", 
-        img : "기프티박스.jpg",
+        img : "giftybox.jpg",
         alt : "thumnail image",
         brand : "기프티박스",
         title : "[무려~울20함유!!] 요플래 와플 스퀘어넥 퍼프 니트",
@@ -212,7 +212,7 @@ export const item = [
         score : "4.4"},
     { 
         url : "#", 
-        img : "엔비룩.jpg",
+        img : "envylook.jpg",
         alt : "thumnail image",
         brand : "엔비룩",
         title : "[추천★] 삐삐와이드밴딩팬츠/기모팬츠/겨울팬츠",
@@ -224,7 +224,7 @@ export const item = [
         score : "4.6"},
     { 
         url : "#", 
-        img : "모디무스.jpg",
+        img : "mosmudy.jpg",
         alt : "thumnail image",
         brand : "모디무스",
         title : "삭스 스판 스웨이드 롱 부츠 [2color]_모디무드",
@@ -236,7 +236,7 @@ export const item = [
         score : "4.6"},
     { 
         url : "#", 
-        img : "캔디글로우.jpg",
+        img : "candygrow.jpg",
         alt : "thumnail image",
         brand : "캔디글로우",
         title : "[주말만 깜짝쿠폰][데일리로 딱이야! 컬러가 예쁜 라운드 니트 ] 소프트 니트",
